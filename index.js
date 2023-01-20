065c594e0eccb75cdd18705d848dc81010931c5d
ad3rinto <aad3rinto@gmail.com>
2023-01-20 20:24:47 +0000

    confused

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-ReactDOM.render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const navbar = (
 	<nav>
 		<h1>My Website</h1>
 		<ul>
-			<l1>Pricing</l1>
+			<li>Pricing</li>
 			<li>About</li>
 			<li>Contact</li>
 		</ul>
-	</nav>,
-	document.getElementById("root")
+	</nav>
 );
+
+ReactDOM.createRoot(document.getElementById("root").render(navbar));
